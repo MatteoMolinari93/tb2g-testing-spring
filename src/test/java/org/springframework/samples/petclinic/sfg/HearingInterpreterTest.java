e0b4850bc0832e2b745661c9eebacda5472cdb08
Molinari <matteomolinari93@gmail.com>
2020-10-15 15:14:15 +0200

    JUnit with Spring ContextConfiguration

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.java
@@ -0,0 +1,23 @@
+package org.springframework.samples.petclinic.sfg;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {BaseConfig.class, LaurelConfig.class})
+public class HearingInterpreterTest {
+	
+	@Autowired
+	HearingInterpreter hearingInterpreter;
+	
+	@Test
+	public void whatIHeard() {
+		assertEquals("Laurel", hearingInterpreter.whatIHeard());
+	}
+
+}
